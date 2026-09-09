f2f916e510472879c26748fca71d1daf70c3d6b5
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-09 09:54:26 +0300

    Work Schedule Beta 0.5: bump app version

--- work-schedule/app/build.gradle.kts
+++ work-schedule/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ilia.workschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0-beta"
+        versionCode = 5
+        versionName = "0.5.0-beta"
     }
 
     buildTypes {
