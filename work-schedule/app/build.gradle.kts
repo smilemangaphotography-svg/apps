a72ed179eec8412a608a17d2eede93ce961d77a4
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-09 13:07:49 +0300

    Bump Work Schedule to 1.0.1 icon fix

--- work-schedule/app/build.gradle.kts
+++ work-schedule/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ilia.workschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
     }
 
     buildTypes {
