3356141b77a7ca1143f41f19c6bb5e1638cc53d7
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-11 01:03:09 +0300

    Bump ShishaLove Merchant iOS runtime to 1.1.2

--- shishalove-ios/Merchant/ShishaLoveMerchantApp.swift
+++ shishalove-ios/Merchant/ShishaLoveMerchantApp.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 @main
 struct ShishaLoveMerchantApp: App {
-    private let startURL = URL(string: "https://shishalove.eu/shishalove-merchant/?app=ios&build=110")!
+    private let startURL = URL(string: "https://shishalove.eu/shishalove-merchant/?app=ios&build=111")!
 
     var body: some Scene {
         WindowGroup {
             ShishaLoveWebView(
                 startURL: startURL,
-                appUserAgent: "ShishaLoveMerchant/1.1.0 iOS"
+                appUserAgent: "ShishaLoveMerchant/1.1.2 iOS"
             )
             .ignoresSafeArea(.keyboard)
             .background(Color.white)
