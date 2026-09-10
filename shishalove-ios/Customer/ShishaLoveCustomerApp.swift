4ab648e941a074b02704053cc1580751f34a9ea1
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-10 18:43:23 +0300

    Point iOS Customer at ShishaLove 1.1.1 runtime

--- shishalove-ios/Customer/ShishaLoveCustomerApp.swift
+++ shishalove-ios/Customer/ShishaLoveCustomerApp.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 @main
 struct ShishaLoveCustomerApp: App {
-    private let startURL = URL(string: "https://shishalove.eu/shishalove-app/?app=ios&build=110")!
+    private let startURL = URL(string: "https://shishalove.eu/shishalove-app/?app=ios&build=111")!
 
     var body: some Scene {
         WindowGroup {
             ShishaLoveWebView(
                 startURL: startURL,
-                appUserAgent: "ShishaLoveCustomer/1.1.0 iOS"
+                appUserAgent: "ShishaLoveCustomer/1.1.1 iOS"
             )
             .ignoresSafeArea(.keyboard)
             .background(Color.white)
