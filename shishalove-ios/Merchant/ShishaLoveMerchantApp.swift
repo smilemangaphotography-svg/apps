1a90d6d0a5f6d987d0be6c702b01512962767172
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-10 17:48:31 +0300

    Align iOS Merchant with ShishaLove 1.1.0

--- shishalove-ios/Merchant/ShishaLoveMerchantApp.swift
+++ shishalove-ios/Merchant/ShishaLoveMerchantApp.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 @main
 struct ShishaLoveMerchantApp: App {
-    private let startURL = URL(string: "https://shishalove.eu/shishalove-merchant/?app=ios&build=100")!
+    private let startURL = URL(string: "https://shishalove.eu/shishalove-merchant/?app=ios&build=110")!
 
     var body: some Scene {
         WindowGroup {
             ShishaLoveWebView(
                 startURL: startURL,
-                appUserAgent: "ShishaLoveMerchant/1.0 iOS"
+                appUserAgent: "ShishaLoveMerchant/1.1.0 iOS"
             )
             .ignoresSafeArea(.keyboard)
             .background(Color.white)
