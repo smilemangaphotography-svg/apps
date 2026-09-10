035ee56169f33612eea97c024e202edf764227a0
smilemangaphotography-svg <smilemangaphotography@gmail.com>
2026-09-10 17:11:45 +0300

    Point iOS Customer at locked 1.1.0 app shell

--- shishalove-ios/Customer/ShishaLoveCustomerApp.swift
+++ shishalove-ios/Customer/ShishaLoveCustomerApp.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 @main
 struct ShishaLoveCustomerApp: App {
-    private let startURL = URL(string: "https://shishalove.eu/shishalove-app/?app=ios&build=100")!
+    private let startURL = URL(string: "https://shishalove.eu/shishalove-app/?app=ios&build=110")!
 
     var body: some Scene {
         WindowGroup {
             ShishaLoveWebView(
                 startURL: startURL,
-                appUserAgent: "ShishaLoveCustomer/1.0 iOS"
+                appUserAgent: "ShishaLoveCustomer/1.1.0 iOS"
             )
             .ignoresSafeArea(.keyboard)
             .background(Color.white)
